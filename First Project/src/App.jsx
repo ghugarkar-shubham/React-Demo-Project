12aaa777b04cccdd84b049dda85fa55afa1e07ac
ghugarkar-shubham <shubhamghugarkar33@gmail.com>
2026-08-16 08:43:30 +0530

    First commit

---
+++ First Project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+const App = () =>{
+
+  return (
+    <>
+      <div id='grand-parent'>
+        <h1>Hello</h1>
+        <h2>Hello</h2>
+      </div>
+      <div id='parent'></div>
+    </>
+  )
+}
+
+export default App
